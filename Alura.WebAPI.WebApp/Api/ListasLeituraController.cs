53ec1fd892c6eae50e5e3ec94885ff4fa19a4f64
vicente_leonardo <vicente@geodados.com.br>
2022-01-07 11:23:43 -0300

    Criação método que retorna lista de leitura Método que retorna capa

---
+++ Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -0,0 +1,57 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("controller")]
+    public class ListasLeituraController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+        private Lista CriaLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All.Where(l => l.Lista == tipo)
+                .Select(x => x.ToApi())
+                .ToList()
+            };
+        }
+
+        [HttpGet]
+        public IActionResult TodasListas()
+        {
+            //Era pra chamar ListaLeitura, mas como o namespace tinha o mesmo nome, foi criado um apelido
+            Lista paraLer = CriaLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriaLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriaLista(TipoListaLeitura.Lendo);
+            var colecao = new List<Lista>
+            {
+                paraLer,
+                lendo,
+                lidos
+            };
+            return Ok(colecao);
+        }
+
+        [HttpGet("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            var lista = CriaLista(tipo);
+            return Ok(lista);
+        }
+
+    }
+}
